8131cae0bf9d882027fb92be486067a250cbc317
Bakhti Rasulov <rasulovbahti@gmail.com>
2022-12-29 17:52:53 -0800

    first commit

---
+++ LearnKorean/LearnKoreanApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnKoreanApp.swift
+//  LearnKorean
+//
+//  Created by Bakhti Rasulov on 12/28/22.
+//
+
+import SwiftUI
+
+@main
+struct LearnKoreanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
